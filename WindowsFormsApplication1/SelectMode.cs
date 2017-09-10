eae07aada48cd2a7cbffaf551689500550d0e0c0
Jakob Wulf-Eck <jakobweck@gmail.com>
2017-09-10 11:24:24 -0500

    Initializing

---
+++ WindowsFormsApplication1/SelectMode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class SelectMode : Form
+    {
+        public SelectMode()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AdminWindow admin = new AdminWindow();
+            admin.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UserWindow user = new UserWindow();
+            user.Show();
+        }
+    }
+}
