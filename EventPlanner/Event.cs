8076136d0625ea1128e0cbc0b8915ee1ed24b92c
Jakob Wulf-Eck <jakobweck@gmail.com>
2017-09-15 14:06:09 -0500

    got project building with austin's Event.cs

--- EventPlanner/Event.cs
+++ EventPlanner/Event.cs
@@ -26,135 +26,137 @@ using System.Windows.Forms;
     -attendees
 */
 #endregion
+class Event
+{
+    #region Private Variables and Properties
+    private int numberOfEvents = 0;
 
-#region Private Variables and Properties
-private int numberOfEvents = 0;
+    private string nameOfEvent = "";
+    private string host = "";
+    private string location = "";
+    private string brief = "";
 
-private string nameOfEvent = "";
-private string host = "";
-private string location = "";
-private string brief = "";
+    private DateTime startTime;
+    private DateTime endTime;
 
-private DateTime startTime;
-private DateTime endTime;
+    private int capacity = 0;
+    private int numberOfAttendees = 0;
+    #endregion
 
-private int capacity = 0;
-private int numberOfAttendees = 0;
-#endregion
-
-private void Event(string hostName)
-{
-    host = hostName;
-}
-
-//SETTERS
-#region Public Method Setters
-public void setName(string name)
-{
-    nameOfEvent = name;
-}
-public void setLocation(string loc)
-{
-    location = loc;
-}
-public void setStart(DateTime start)
-{
-    startTime = start;
-}
-public void setEnd(DateTime end)
-{
-    endTime = end;
-}
-public void setBrief(string msg)
-{
-    brief = msg;
-}
-public void setCapacity(int cap)
-{
-    capacity = cap;
-}
-#endregion
-
-/*
-NOTE:Maybe we'll eventually show everyone who is going to said event?
-*/
-public void addAttenedee()
-{
-    numberOfAttendees++;
-}
-
-/*
-NOTE: WIP Random Event ID
-*/
-
-//GETTERS
-#region Public Method Getters
-public string getName()
-{
-    return (nameOfEvent);
-}
-public string getHost()
-{
-    return (host);
-}
-public void getLocation()
-{
-    return (location);
-}
-public DateTime getStart()
-{
-    return (startTime);
-}
-public DateTime getEnd()
-{
-    return (endTime);
-}
-public string getBrief()
-{
-    return (brief);
-}
-public int getCapacity()
-{
-    return (capacity);
-}
-public int getAttendeeCount()
-{
-    return (numberOfAttendees);
-}
-#endregion
-
-#region File Functionality
-public void saveToFile(string eventName, string capacity, string briefMsg, string startTime, string endTime)
-{
-    string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/eventSaveFile.txt";
-
-    if (numberOfEvents == 0)
+    public Event(string hostName)
     {
-        // Write the string to a file.
-        System.IO.StreamWriter file = new System.IO.StreamWriter(path);
+        host = hostName;
     }
-    else
+
+    //SETTERS
+    #region Public Method Setters
+    public void setName(string name)
     {
-        System.IO.StreamWriter file = System.IO.StreamWriter(path);
+        nameOfEvent = name;
     }
-    numberOfEvents++;
-    
-    //Traverses To A New Line
+    public void setLocation(string loc)
+    {
+        location = loc;
+    }
+    public void setStart(DateTime start)
+    {
+        startTime = start;
+    }
+    public void setEnd(DateTime end)
+    {
+        endTime = end;
+    }
+    public void setBrief(string msg)
+    {
+        brief = msg;
+    }
+    public void setCapacity(int cap)
+    {
+        capacity = cap;
+    }
+    #endregion
 
-    file.WriteLine(eventName, ",", capacity, ",", briefMsg, ",", startTime, ",", endTime);
-    file.Close();
+    /*
+    NOTE:Maybe we'll eventually show everyone who is going to said event?
+    */
+    public void addAttenedee()
+    {
+        numberOfAttendees++;
+    }
+
+    /*
+    NOTE: WIP Random Event ID
+    */
+
+    //GETTERS
+    #region Public Method Getters
+    public string getName()
+    {
+        return (nameOfEvent);
+    }
+    public string getHost()
+    {
+        return (host);
+    }
+    public string getLocation()
+    {
+        return (location);
+    }
+    public DateTime getStart()
+    {
+        return (startTime);
+    }
+    public DateTime getEnd()
+    {
+        return (endTime);
+    }
+    public string getBrief()
+    {
+        return (brief);
+    }
+    public int getCapacity()
+    {
+        return (capacity);
+    }
+    public int getAttendeeCount()
+    {
+        return (numberOfAttendees);
+    }
+    #endregion
+
+    #region File Functionality
+    public void saveToFile(string eventName, string capacity, string briefMsg, string startTime, string endTime)
+    {
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/eventSaveFile.txt";
+        System.IO.StreamWriter file;
+        if (numberOfEvents == 0)
+        {
+            // Write the string to a file.
+             file = new System.IO.StreamWriter(path);
+        }
+        else
+        {
+             file = new System.IO.StreamWriter(path);
+        }
+        numberOfEvents++;
+
+        //Traverses To A New Line
+
+        file.WriteLine(eventName, ",", capacity, ",", briefMsg, ",", startTime, ",", endTime);
+        file.Close();
+    }
+
+    /*
+     * Note: this will change number of attendees for an event in file 
+    */
+    public void quickSaveToFile()
+    {
+
+    }
+
+    public void deleteEventFromFile(string matchName)
+    {
+
+    }
+    #endregion
 }
-
-/*
- * Note: this will change number of attendees for an event in file 
-*/
-public void quickSaveToFile()
-{
-
-}
-
-public void deleteEventFromFile(string matchName)
-{
-
-}
-#endregion
